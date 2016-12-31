e65021151e748f07f58b62493d370291ef9bf892
Christian Antila <tagged-github.com@chille.se>
2016-12-31 05:49:50 +0100

    Imported common code

---
+++ lumen/app/Traits/EntityStandardFiltering.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Traits;
+
+trait EntityStandardFiltering
+{
+	/**
+	 *
+	 */
+	protected function _applyStandardFilters($model, $request)
+	{
+		// Paging filter
+		$filters = [
+			"per_page" => $this->per_page($request), // TODO: Rename?
+		];
+
+		// Filter on relations
+		if(!empty($request->get("relations")))
+		{
+			$filters["relations"] = $request->get("relations");
+		}
+
+		// Filter on search
+		if(!empty($request->get("search")))
+		{
+			$filters["search"] = $request->get("search");
+		}
+
+		// Sorting
+		if(!empty($request->get("sort_by")))
+		{
+			$order = ($request->get("sort_order") == "desc" ? "desc" : "asc");
+			$filters["sort"] = [$request->get("sort_by"), $order];
+		}
+
+		// Filter by id
+		if(!empty($request->get("ids")))
+		{
+			$ids = explode(",", $request->get("ids"));
+			$filters["ids"] = $ids;
+		}
+
+		// Load data from database
+		$result = call_user_func("\App\Models\\{$model}::list", $filters);
+
+		// Return json array
+		return Response()->json($result, 201);
+	}
+}
