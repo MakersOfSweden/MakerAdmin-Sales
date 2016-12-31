9b0e4545f33f7acac7568b7859ad4f90bd731e52
Christian Antila <tagged-github.com@chille.se>
2016-12-31 05:48:51 +0100

    Database migrations for products and subscriptions

---
+++ lumen/database/migrations/2016_12_31_013934_create_sales_product_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesProductTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create("sales_product", function (Blueprint $table)
+		{
+			$table->increments("sales_product_id");
+			$table->string("title");
+			$table->text("description")->nullable();
+
+			$table->dateTimeTz("expiry_date")->nullable();
+			$table->string("auto_extend");
+			$table->integer("price");
+			$table->integer("interval");
+
+			$table->dateTimeTz("created_at")->default(DB::raw("CURRENT_TIMESTAMP"));
+			$table->dateTimeTz("updated_at")->default(DB::raw("CURRENT_TIMESTAMP"));
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop("sales_product");
+	}
+}
---
+++ lumen/database/migrations/2016_12_31_013946_create_sales_subscription_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesSubscriptionTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create("sales_subscription", function (Blueprint $table)
+		{
+			$table->increments("sales_subscription_id");
+			$table->string("title");
+//			$table->text("description")->nullable();
+
+			$table->dateTimeTz("date_start");
+			$table->integer("product_id"); // foreign key
+
+			$table->dateTimeTz("created_at")->default(DB::raw("CURRENT_TIMESTAMP"));
+			$table->dateTimeTz("updated_at")->default(DB::raw("CURRENT_TIMESTAMP"));
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop("sales_subscription");
+	}
+}
